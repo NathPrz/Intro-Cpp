513d9ed446060232d854db4f60aa21fd1ca538a0
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-09-15 09:13:39 +0000

    Upload New File

---
+++ tinycar/mission1/question1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+int age=20; 
+int i=0 ; i=1+1;
+int j=i ;
+string voiture= "peugeot" ; 
+
+    cout <<"la variable age de type int contient 20 "<<endl;
+    cout <<" i de type int contient 0"<<endl;
+    cout <<" j de type int contient i "<<endl;
+    cout <<"Finalement la variable voiture de type string contient peugeot"<<endl;
+    }
