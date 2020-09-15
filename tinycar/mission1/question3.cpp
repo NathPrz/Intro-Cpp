53ed8f19ceaac617db91d170bf3c2228b9b92501
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-09-15 21:41:01 +0000

    Upload New File

---
+++ tinycar/mission1/question3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){  
+  int prixVoitureHT;
+  cout<<"quel est le prix HT de votre voiture?";
+  cin>> prixVoitureHT;
+  double tauxTVA = 0.2;
+  double prixTTC = prixVoitureHT*(1+tauxTVA);
+    cout << "Le prix totale de la voiture TTC est " ;
+    cout << prixTTC ;
+    
+}
