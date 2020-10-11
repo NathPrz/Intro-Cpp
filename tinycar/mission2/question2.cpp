4648266eba7f109cb0b5252cf4f754eecdb5b8ea
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-10-11 16:54:15 +0000

    Update question2.cpp

--- tinycar/mission2/question2.cpp
+++ tinycar/mission2/question2.cpp
@@ -26,23 +26,32 @@ int main(){
   }
 
   //Taux TVA different si la voiture est éléctrique
-
-  if (voitureElectrique == 1) {
+  
+  double prixTTC;{
+   if (voitureElectrique == 1) {
     double tauxTVA = 0.05;
     double prixTTC = prixVoitureHT*(1+tauxTVA);
     cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
-    cout << prixTTC << endl; 
-  }else{
+    cout << prixTTC << endl;
+     //Remise de 10% si l'achat TTC est superieur à 20 000€
+    double remise = 0.1;    
+    if (prixTTC > 20000) {
+     double prixAvecRemise = prixTTC*(1-remise);
+      cout << "Le prix de la voiture avec remise est ";
+      cout << prixAvecRemise<<endl; 
+    }   
+   }else{
     double tauxTVA = 0.2;
     double prixTTC = prixVoitureHT*(1+tauxTVA);
     cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
     cout << prixTTC << endl; 
-  //Remise de 10% si l'achat TTC est superieur à 20 000€
-   double remise = 0.1;
+     //Remise de 10% si l'achat TTC est superieur à 20 000€
+    double remise = 0.1;
     if (prixTTC > 20000) {
      double prixAvecRemise = prixTTC*(1-remise);
       cout << "Le prix de la voiture avec remise est ";
-      cout << prixAvecRemise<<endl;
-    }
+      cout << prixAvecRemise<<endl; 
     }   
+   }
+}
 }
