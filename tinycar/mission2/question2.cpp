6fb1dc46a64709301ad861980e168ccc7676ac14
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-10-13 09:18:16 +0000

    Update question2.cpp

--- tinycar/mission2/question2.cpp
+++ tinycar/mission2/question2.cpp
@@ -25,33 +25,26 @@ int main(){
   cin>> voitureElectrique;
   }
 
-  //Taux TVA different si la voiture est éléctrique
+  //Prix TTC. Taux TVA different si la voiture est éléctrique.
   
-  double prixTTC;{
+  double prixTTC;
    if (voitureElectrique == 1) {
     double tauxTVA = 0.05;
-    double prixTTC = prixVoitureHT*(1+tauxTVA);
-    cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
-    cout << prixTTC << endl;
-     //Remise de 10% si l'achat TTC est superieur à 20 000€
-    double remise = 0.1;    
-    if (prixTTC > 20000) {
-     double prixAvecRemise = prixTTC*(1-remise);
-      cout << "Le prix de la voiture avec remise est ";
-      cout << prixAvecRemise<<endl; 
-    }   
-   }else{
-    double tauxTVA = 0.2;
-    double prixTTC = prixVoitureHT*(1+tauxTVA);
+    prixTTC = prixVoitureHT*(1+tauxTVA);
     cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
     cout << prixTTC << endl; 
-     //Remise de 10% si l'achat TTC est superieur à 20 000€
+   }else{
+    double tauxTVA = 0.2;
+    prixTTC = prixVoitureHT*(1+tauxTVA);
+    cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
+    cout << prixTTC << endl;    
+   }
+
+   //Remise de 10% si l'achat TTC est superieur à 20 000€
     double remise = 0.1;
     if (prixTTC > 20000) {
      double prixAvecRemise = prixTTC*(1-remise);
       cout << "Le prix de la voiture avec remise est ";
       cout << prixAvecRemise<<endl; 
-    }   
-   }
 }
 }
