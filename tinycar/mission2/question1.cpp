a680791b6d1e552d24af52fe1d8e9c67f7073202
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-10-13 09:17:24 +0000

    Update question1.cpp

--- tinycar/mission2/question1.cpp
+++ tinycar/mission2/question1.cpp
@@ -15,20 +15,23 @@ int main(){
   cin>> modelVoiture;
   }
   
-  int prixVoitureHT;{
+  int prixVoitureHT;
   cout<<"quel est le prix HT de votre voiture?";
   cin>> prixVoitureHT;
+  
+  //Prix TTC
+  
   double tauxTVA = 0.2;
   double prixTTC = prixVoitureHT*(1+tauxTVA);
     cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
-    cout << prixTTC ;
+    cout << prixTTC<<endl;
+    
+  //Remise de 10% si l'achat TTC est superieur à 20 000€
 
   double remise = 0.1;
-    if (prixTTC > 20000) {
-     double prixAvecRemise = prixTTC*(1-remise);
-      cout << " Le prix de la voiture avec remise est ";
-      cout << prixAvecRemise;
-    
-  }
+  if (prixTTC > 20000) {
+    double prixAvecRemise = prixTTC*(1-remise);
+    cout << " Le prix de la voiture avec remise est ";
+    cout << prixAvecRemise;
   }
 }
