f9666da540e9eee39ede1d285c8acb0562f16c71
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-11-03 11:19:36 +0000

    Update question4.cpp

--- tinycar/mission1/question4.cpp
+++ tinycar/mission1/question4.cpp
@@ -5,23 +5,21 @@ using namespace std;
 
 int main(){ 
   
-  string marqueVoiture;{
+  string marqueVoiture;
   cout<<"quelle est la marque de la voiture?";
   cin>> marqueVoiture;
-  }
   
-  string modelVoiture;{
+  string modelVoiture;
   cout<<"quel est le model de la voiture?";
   cin>> modelVoiture;
-  }
   
-  int prixVoitureHT;{
+  int prixVoitureHT;
   cout<<"quel est le prix HT de votre voiture?";
   cin>> prixVoitureHT;
+
   double tauxTVA = 0.2;
+  
   double prixTTC = prixVoitureHT*(1+tauxTVA);
     cout << "Le prix d'une voiture " + marqueVoiture + " model " + modelVoiture + " TTC est " ;
-    cout << prixTTC ;
-    }
-    
+    cout << prixTTC ;    
 }
