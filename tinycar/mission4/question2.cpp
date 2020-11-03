7c8dc2c42e470a53d5a384007ac2cd8a394d2183
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-11-03 13:15:34 +0000

    Upload New File

---
+++ tinycar/mission4/question2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Programme pour gérer un panier
+
+int main()
+{
+
+    //Tableau pour les noms des accesoires
+    string accesoires[5] = {"House Clavier ", "Pedale ", "Métronome ", "Pupitre ", "Accordeur "};
+    //Tableau pour les prix hors taxes des accesoires
+    double prixHT[5];
+
+    
+    //Boucle qui demande à l'utilisateur le prixHT des accesoires pour inisialiser le deuxième tableau
+    cout << "Veulliez indiquer le prix hors taxes de chaque accesoire, s'il vous plait:" << endl;
+    
+    for (int indice = 0; indice < 5; indice++){
+        cout << accesoires[indice] << endl;
+        cin >> prixHT[indice];
+    }
+
+    //Boucle qui affiche le nom et le prixHT des accesoires
+    for(int indice = 0; indice < 5; indice++){
+    cout << accesoires[indice];
+    cout << prixHT[indice] << endl;
+    }
+
+    return 0;
+}
