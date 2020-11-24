cd7cecb93480a48cb81fb6de0b0cc252ef2759c1
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-11-24 09:48:46 +0000

    Upload New File

---
+++ tinycar/mission5/question1.cpp
@@ -0,0 +1,51 @@
+//Programme pour gérer un panier
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Procédure qui affiche un texte descriptif pour chaque accesoire.
+Une variable string et une double sont passés en paramètres*/
+void texteDescriptif (string nom, double prix)
+{
+    cout << "L'accesoire " << nom << " cout " << prix << " hors taxes.\n";
+}
+
+//Programme pour gérer un panier
+
+int main()
+{
+    //Inisialisation des pointeurs
+    string* accesoires = nullptr;
+    double* prixHT = nullptr;
+
+    //Definition de la taille des tableaux à l'aide de l'utilisateur
+    int quantiteDAccesoires;
+    cout << "Bonjour! Combien d'accesoires voulez vous ajouter au panier?";
+    cin >> quantiteDAccesoires;
+
+    accesoires = new string[quantiteDAccesoires];
+    prixHT = new (nothrow) double[quantiteDAccesoires];
+
+    //Boucle qui demande à l'utilisateur les accesoires et leur prix pour inisialiser les tableaux
+    cout << "Veuillez indiquer l'information qui suive pour chaque accesoire:" << endl;
+    for (int indice = 0; indice < quantiteDAccesoires; indice++)
+    {
+        cout << "Nom: " << endl;
+        //cin >> accesoires[indice]; NE MARCHE PAS avec d'espaces vides
+        cin.ignore();
+        getline(cin, accesoires[indice]);
+        cout << "Prix HT: " << endl;
+        cin >> prixHT[indice];
+    }
+
+    for (int indice = 0; indice < quantiteDAccesoires; indice++)
+    {
+        //Boucle qui appelle la fonction pour afficher le descriptif du panier
+        texteDescriptif(accesoires[indice], prixHT[indice]);
+    }
+
+    //Suppression de pointeurs
+    delete[] accesoires;
+    delete[] prixHT;
+    return 0;
+}
