7ee461f57d7bad8fb9195b7dd2c77bdae7eb3646
Nathalia Perez Ramirez <nathalia.perez.r@gmail.com>
2020-11-24 15:20:43 +0000

    Upload New File

---
+++ tinycar/mission5/question2.cpp
@@ -0,0 +1,49 @@
+//Programme pour gérer un panier
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Fonction qui calcule le TVA pour chaque accesoire.//
+double calculePrixTTC(double prixHT)
+{
+    double tva = 0.2;
+    return prixHT * (1 + tva); 
+}
+
+//Programme pour gérer un panier
+
+int main()
+{
+    //Inisialisation des pointeurs
+    string *accesoires = nullptr;
+    double *prixHT = nullptr;
+    double *prixTTC = nullptr;
+
+    //Definition de la taille des tableaux à l'aide de l'utilisateur
+    int quantiteDAccesoires;
+    cout << "Bonjour! Combien d'accesoires voulez vous ajouter au panier?";
+    cin >> quantiteDAccesoires;
+
+    accesoires = new string[quantiteDAccesoires];
+    prixHT = new (nothrow) double[quantiteDAccesoires];
+    prixTTC = new (nothrow) double[quantiteDAccesoires];
+
+    //Boucle qui demande à l'utilisateur les accesoires et leur prix pour inisialiser les tableaux
+    cout << "Veuillez indiquer l'information qui suive pour chaque accesoire:" << endl;
+    for (int indice = 0; indice < quantiteDAccesoires; indice++)
+    {
+        cout << "Nom: " << endl;
+        //cin >> accesoires[indice]; NE MARCHE PAS avec d'espaces vides
+        cin.ignore();
+        getline(cin, accesoires[indice]);
+        cout << "Prix HT: " << endl;
+        cin >> prixHT[indice];
+        prixTTC[indice]=calculePrixTTC(prixHT[indice]);
+    }
+
+    //Suppression de pointeurs
+    delete[] accesoires;
+    delete[] prixHT;
+    delete[] prixTTC;
+    return 0;
+}
